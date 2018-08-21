f49b6710271e774beee62e4b3119e7f5b265a90f
jayhatha <jay@jayhathaway.com>
2018-08-20 18:34:10 -0700

    ui fixes and building out picks interface

--- controllers/picks.js
+++ controllers/picks.js
@@ -7,22 +7,23 @@ var isTeamOwner = require('../middleware/isTeamOwner');
 
 // /picks/:id - creates a pick for a team for the selected week, so we can add queens to it
 
-router.post('/:id', isLoggedIn, function(req, res) {
-  console.log("reqparamsid is " + req.params.id);
-  db.team.findById(req.params.id)
-  .then(function(team) {
-      db.pick.findOrCreate({
-        where: { weekId: req.body.week, queenId: req.body.queen }
-      }).spread(function(pick, created) {
-        team.addPick(pick).then(function(data) {
-          console.log('well, we added a pick with a week and a team, I guess???');
-          res.redirect('/teams/' + req.params.id + '/manage');
-        });
-      });
-    }).catch(function(error) {
-      res.status(400).render('main/404');
-    });
+router.get('/', function (req, res) {
+  res.render('picks');
 });
 
+router.post('/:id', isLoggedIn, function (req, res) {
+  db.team.findById(req.params.id)
+  .then(function (team) {
+    db.pick.findOrCreate({
+      where: { weekId: req.body.week, queenId: req.body.queen }
+    }).spread(function (pick, created) {
+      team.addPick(pick).then(function (data) {
+        res.redirect('/teams/' + req.params.id + '/manage');
+      });
+    });
+  }).catch(function (error) {
+    res.status(400).render('main/404');
+  });
+});
 
 module.exports = router;
